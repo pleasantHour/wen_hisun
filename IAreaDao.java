8183cc949aa3340e0acc912b029112d365dbd13e
wen <wangyiemailwxm@163.com>
2019-03-01 14:29:04 +0800

    second

---
+++ IAreaDao.java
@@ -0,0 +1,15 @@
+///*
+// * @ClassName IAreaDao
+// * @Description
+// * @version 1.0
+// * @Date 2019-03-01 09:52:04
+// */
+//package com.cmpay.study.dao;
+//
+//import com.cmpay.lemon.framework.dao.BaseDao;
+//import com.cmpay.study.entity.AreaDO;
+//import org.apache.ibatis.annotations.Mapper;
+//
+//@Mapper
+//public interface IAreaDao extends BaseDao<AreaDO, String> {
+//}
